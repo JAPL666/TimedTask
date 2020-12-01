6dc1cd7a8bc3a2e4b77f05f1f54a342167af8553
苦艾酒 <37876529+JAPL666@users.noreply.github.com>
2020-12-01 16:18:29 +0800

    定时任务

--- src/main/java/com/warma/timed_task/TimedTaskApplication.java
+++ src/main/java/com/warma/timed_task/TimedTaskApplication.java
@@ -1,15 +1,8 @@
 package com.warma.timed_task;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -18,30 +11,5 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class TimedTaskApplication {
     public static void main(String[] args) {
         SpringApplication.run(TimedTaskApplication.class, args);
-    }
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(redirectConnector());
-        return tomcat;
-    }
-
-    private Connector redirectConnector() {
-        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-        connector.setScheme("http");
-        connector.setPort(1314);
-        connector.setSecure(false);
-        connector.setRedirectPort(443);
-        return connector;
     }
 }
