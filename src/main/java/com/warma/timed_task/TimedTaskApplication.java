c4a9126d6061ae57b02e82b9c1e3ce7598bc89c1
苦艾酒 <37876529+JAPL666@users.noreply.github.com>
2021-01-22 10:54:47 +0800

    定时任务

--- src/main/java/com/warma/timed_task/TimedTaskApplication.java
+++ src/main/java/com/warma/timed_task/TimedTaskApplication.java
@@ -1,6 +1,5 @@
 package com.warma.timed_task;
 
-import com.warma.timed_task.app.CloudLearning;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
